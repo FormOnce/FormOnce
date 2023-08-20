4e834224474b8ba0eff3ab21abff24bf550f677b
SUSHANT KUMAR <ksushant6566@gmail.com>
2023-08-20 08:51:23 +0530

    chore: remove unused imports

--- @/components/dashboardLayout.tsx
+++ @/components/dashboardLayout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Image from "next/image";
 
 import { MainNav, UserNav, TeamSwitcher } from "@/components/ui/nav";
 import RootLayout, { type RootLayoutProps } from "./rootLayout";
