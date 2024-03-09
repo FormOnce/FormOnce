16533b315e60b7d78bd98834e26bc2845f2819c7
ksushant6566 <ksushant6566@gmail.com>
2024-03-09 23:30:27 +0530

    feat: add functionality to reorder questions

--- src/server/api/routers/forms/dtos/updateForm.ts
+++ src/server/api/routers/forms/dtos/updateForm.ts
@@ -4,4 +4,5 @@ export const ZUpdateForm = z.object({
     id: z.string(),
     name: z.string().optional(),
     formSchema: z.object({}).passthrough().optional(),
+    questions: z.array(z.object({}).passthrough()).optional(),
 });
