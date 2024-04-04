33c8c008b2f142e095e83353c34777a07b81c5a7
ksushant6566 <ksushant6566@gmail.com>
2024-04-04 08:55:05 +0530

    feat: add delete webhook btn with confirmation dialog

--- src/pages/settings/webhooks.tsx
+++ src/pages/settings/webhooks.tsx
@@ -1,7 +1,18 @@
 import DashboardLayout from "~/layouts/dashboardLayout";
 import { Button } from "~/components/ui/button";
 import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
   Icons,
+  Input,
+  Label,
   Switch,
   Table,
   TableBody,
@@ -16,9 +27,11 @@ import {
   TooltipTrigger,
 } from "~/components/ui";
 import { api } from "~/utils/api";
-import { Loader2 } from "lucide-react";
+import { Edit, Loader2 } from "lucide-react";
 import { AddWebhookDialog } from "~/components/webhook/add-webhook-dialog";
 import { toast } from "sonner";
+import type { Webhook } from "@prisma/client";
+import { useState } from "react";
 
 export default function Webhooks() {
   return (
@@ -77,17 +90,16 @@ const WebhookTable = () => {
       <TableHeader>
         <TableRow>
           <TableHead>Webhook</TableHead>
-          <TableHead>Event</TableHead>
+          <TableHead>Events</TableHead>
           <TableHead>URL</TableHead>
-          <TableHead>Secret</TableHead>
-          <TableHead>Created</TableHead>
+          {/* <TableHead>Created</TableHead> */}
           <TableHead>Enabled</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {isLoading ? (
           <TableRow>
-            <TableCell colSpan={6}>
+            <TableCell colSpan={4}>
               <div className="my-4 flex justify-center">
                 <Loader2 className="animate-spin" />{" "}
               </div>
@@ -96,19 +108,27 @@ const WebhookTable = () => {
         ) : (
           webhooks?.map((webhook) => (
             <TableRow key={webhook.id}>
-              <TableCell>{webhook.name}</TableCell>
+              <TableCell className="max-w-[12rem] truncate">
+                <p>
+                  {webhook.name}
+                  <br />
+                  <span className="text-xs text-muted-foreground">
+                    {" "}
+                    {webhook.id}
+                  </span>
+                </p>
+              </TableCell>
               <TableCell>
-                <ul className="capitalize">
+                <ul className="text-xs capitalize">
                   {webhook.events.map((event) => (
                     <li key={event}>{event.replace("_", " ").toLowerCase()}</li>
                   ))}
                 </ul>
               </TableCell>
-              <TableCell className="max-w-[15rem] truncate" title={webhook.url}>
+              <TableCell className="max-w-[12rem] truncate" title={webhook.url}>
                 {webhook.url}
               </TableCell>
-              <TableCell>{webhook.secret}</TableCell>
-              <TableCell>{webhook.createdAt.toLocaleDateString()}</TableCell>
+              {/* <TableCell>{webhook.createdAt.toLocaleDateString()}</TableCell> */}
               <TableCell>
                 <TooltipProvider>
                   <Tooltip>
@@ -126,10 +146,87 @@ const WebhookTable = () => {
                   </Tooltip>
                 </TooltipProvider>
               </TableCell>
+              <TableCell className="flex gap-2">
+                <DeleteWebhookDialog refetch={refetch} webhook={webhook} />
+                <Button
+                  variant="secondary"
+                  size="icon"
+                  className="hover:bg-destructive/90 hover:text-destructive-foreground"
+                >
+                  <Edit className="h-4 w-4" />
+                </Button>
+              </TableCell>
             </TableRow>
           ))
         )}
       </TableBody>
     </Table>
+  );
+};
+
+type DeleteWebhookDialogProps = {
+  webhook: Webhook;
+  refetch: () => Promise<unknown>;
+};
+const DeleteWebhookDialog = ({
+  refetch,
+  webhook,
+}: DeleteWebhookDialogProps) => {
+  const { mutateAsync: deleteWebhook, isLoading } =
+    api.webhook.delete.useMutation();
+
+  const onDeleteWebhook = async () => {
+    await deleteWebhook({ id: webhook.id });
+    toast.success("Webhook deleted", {
+      duration: 3000,
+      position: "top-center",
+    });
+    await refetch();
+  };
+
+  const [value, setValue] = useState("");
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Button
+          variant="secondary"
+          size="icon"
+          className="hover:bg-destructive/90 hover:text-destructive-foreground"
+        >
+          <Icons.trash className="h-5 w-5" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent className="min-w-max">
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Are you sure you want to delete webhook {webhook.name} ?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <div className="my-4 space-y-2">
+          <Label className="flex items-center">
+            <span className="text-sm">Confirm by typing : </span>
+            <span className="mx-1 text-base text-red-500">
+              Delete {webhook.name}{" "}
+            </span>{" "}
+          </Label>
+          <Input onChange={(e) => setValue(e.target.value)} />
+        </div>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={onDeleteWebhook}
+            disabled={value !== `Delete ${webhook.name}`}
+            className="bg-destructive/90 text-destructive-foreground hover:bg-destructive"
+          >
+            {isLoading ? <Loader2 className="animate-spin" /> : "Delete"}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
