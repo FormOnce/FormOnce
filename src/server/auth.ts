89ef4288da06992a8a138f052bc89b7b74c8c395
ksushant6566 <ksushant6566@gmail.com>
2024-05-09 00:43:54 +0530

    feat: add allowDangerousEmailAccountLinking in next-auth googleProvider

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -125,6 +125,7 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+      allowDangerousEmailAccountLinking: true,
     }),
     /**
      * 
