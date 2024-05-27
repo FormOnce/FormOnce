694ed4091398362b5a84d6423c73b05f055b3bae
Sushant kumar <ksushant6566@gmail.com>
2024-05-27 06:04:28 +0530

    feat: add landing page v_1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,21 @@
+import {
+  GitHubLogoIcon,
+  PinRightIcon,
+  TwitterLogoIcon,
+} from '@radix-ui/react-icons'
+import { ChevronRight, CopyrightIcon } from 'lucide-react'
 import type { GetServerSideProps } from 'next'
 import { signOut } from 'next-auth/react'
 import Head from 'next/head'
+import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
-import { Button, buttonVariants } from '~/components/ui/button'
-import { Icons } from '~/components/ui/icons'
+import { Icons, Input } from '~/components/ui'
+import { Button } from '~/components/ui/button'
 import { getServerAuthSession } from '~/server/auth'
-import { cn } from '~/utils/cn'
+
+import HeroImg from '../assets/hero.png'
 
 export default function Home({ id }: { id: string }) {
   const router = useRouter()
@@ -34,13 +42,13 @@ export default function Home({ id }: { id: string }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="dark min-h-screen bg-background">
-        <nav className="sticky top-0 flex items-center justify-between border px-12 py-4 text-foreground">
+        <nav className="sticky top-0 flex items-center justify-between border-b px-12 py-4 text-foreground z-50 backdrop-blur-md">
           <Link href="/">
             <span className="text-2xl font-extrabold text-[hsl(280,100%,70%)]">
               FormOnce
             </span>
           </Link>
-          {id ? (
+          {/* {id ? (
             <div className="flex">
               <Button onClick={handleRedirectToDashboard} variant="ghost">
                 Dashboard
@@ -59,63 +67,111 @@ export default function Home({ id }: { id: string }) {
             >
               <span className="text-foreground">Sign in</span>
             </Link>
-          )}
+          )} */}
+          <Button variant={'link'}>
+            <Link href="https://github.com/FormOnce/FormOnce">
+              <GitHubLogoIcon width={'28'} height={'28'} />
+            </Link>
+          </Button>
         </nav>
         <div className="flex flex-col items-center justify-center bg-background">
-          <div className="container flex flex-col items-center gap-16 px-4 py-16">
-            <h1 className="text-center text-6xl font-extrabold leading-none tracking-tight text-foreground">
-              Create your{' '}
-              <span className="text-[hsl(280,100%,70%)]">FormOnce</span> and use
-              it anywhere
-            </h1>
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-8">
-              <Link
-                className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-                href="https://create.t3.gg/en/usage/first-steps"
-                target="_blank"
-              >
-                <h3 className="text-2xl font-bold">Create</h3>
-                <div className="text-lg">
-                  Create forms using our visual editor
-                </div>
-                <br />
-                <div className="text-lg">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Consequatur voluptates placeat voluptatem, harum, minima,
-                </div>
+          <div className="container flex flex-col items-center gap-16 px-4 py-16 max-w-5xl">
+            <div className="flex flex-col items-center gap-6">
+              <h1 className="text-center text-7xl font-extrabold leading-none tracking-tight text-foreground">
+                {/* <span className="text-[hsl(280,100%,70%)]">FormOnce</span> */}
+                Forms that Speak, Literally!
+              </h1>
+              <h2 className="text-center text-3xl font-base text-foreground max-w-2xl">
+                <span className="text-[hsl(280,100%,70%)] font-extrabold">
+                  FormOnce
+                </span>{' '}
+                is the #1 open-source platform for building voice & video
+                enabled forms.
+              </h2>
+              <h2>
+                <i className="text-xl font-base text-foreground max-w-2xl">
+                  better than Typeform, Tallyforms, VideoAsk and everything
+                  else.
+                </i>
+              </h2>
+            </div>
+            <div className="flex justify-center items-center flex-col gap-4">
+              <div className="relative">
+                <Input
+                  placeholder="Enter your email"
+                  className="w-[360px] py-1 px-3 text-primary text-sm h-12"
+                />
+                <Button className="absolute right-1.5 h-7 top-2.5">
+                  Join waitlist
+                </Button>
+              </div>
+              <Link href="https://github.com/FormOnce/FormOnce">
+                <Button variant={'secondary'}>
+                  🌟 Star FormOnce on{' '}
+                  <GitHubLogoIcon className="ml-2" width={'20'} height={'20'} />{' '}
+                  <ChevronRight className="ml-4" size={'12'} />
+                </Button>
               </Link>
-              <Link
-                className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-                href="https://create.t3.gg/en/usage/first-steps"
-                target="_blank"
-              >
-                <h3 className="text-2xl font-bold">Share</h3>
-                <div className="text-lg">Share with your users</div>
-                <br />
-                <div className="text-lg">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Consequatur voluptates placeat voluptatem, harum, minima,
-                </div>
-              </Link>
-              <Link
-                className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-                href="https://create.t3.gg/en/usage/first-steps"
-                target="_blank"
-              >
-                <h3 className="text-2xl font-bold">Embed</h3>
-                <div className="text-lg">
-                  Embed as a react component in your own code base
-                </div>
-                <br />
-                <div className="text-lg">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Consequatur voluptates placeat voluptatem, harum, minima,
-                </div>
-              </Link>
+            </div>
+            <div className="hidden md:flex justify-center border rounded-xl shadow-xl ring-1 ring-gray-900/10 overflow-hidden mb-12 ">
+              <Image
+                src={HeroImg}
+                alt="FormOnce"
+                width={1200}
+                height={600}
+                className="object-cover"
+              />
             </div>
           </div>
         </div>
       </main>
+      <footer className="flex items-center justify-center dark bg-background">
+        <div className="flex gap-6 w-[80%] border border-b-0 p-12 rounded-tr-xl rounded-tl-xl md:justify-between">
+          <div className="space-y-2">
+            <div className="flex gap-1 items-end">
+              <span className="text-[hsl(280,100%,70%)] font-extrabold text-3xl">
+                FormOnce
+              </span>
+              <Link
+                href="https://github.com/ksushant6566"
+                className="flex items-center justify-center"
+                target="_blank"
+                rel="noopener noreferrer"
+              ></Link>
+            </div>
+            <p className="flex gap-2 items-center text-muted-foreground text-sm max-w-md">
+              <CopyrightIcon width={'14'} /> Formonce 2024 | Made with 💙
+            </p>
+          </div>
+          <div className="flex flex-col justify-start space-y-2">
+            <h2 className="text-muted-foreground">Get in touch with us</h2>
+            <div className="flex gap-4 px-2">
+              <Button
+                variant={'link'}
+                className="ring-1 rounded-full p-2.5 grayscale hover:grayscale-0"
+              >
+                <Link
+                  href="https://github.com/FormOnce/FormOnce"
+                  className="flex items-center"
+                >
+                  <GitHubLogoIcon width={'16'} height={'16'} />
+                </Link>
+              </Button>
+              <Button
+                variant={'link'}
+                className="ring-1 rounded-full p-2.5 grayscale hover:grayscale-0"
+              >
+                <Link
+                  href="https://x.com/form_once"
+                  className="flex items-center"
+                >
+                  <TwitterLogoIcon width={'16'} height={'16'} />
+                </Link>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
